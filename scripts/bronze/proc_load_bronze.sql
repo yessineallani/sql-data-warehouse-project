c2651f9060b0d66521ca20fb1e6e311cd23319ed
yessineallani <yessineallani4@gmail.com>
2025-11-16 22:16:37 +0100

    Create proc_load_bronze.sql

---
+++ scripts/bronze/proc_load_bronze.sql
@@ -0,0 +1,12 @@
+INSERT INTO bronze.DimCountry (country_code, country_name)
+VALUES
+('US', 'United States'),
+('GB', 'United Kingdom'),
+('CA', 'Canada'),
+('FR', 'France'),
+('DE', 'Germany'),
+('JP', 'Japan'),
+('KR', 'South Korea'),
+('RU', 'Russia'),
+('IN', 'India'),
+('MX', 'Mexico');
