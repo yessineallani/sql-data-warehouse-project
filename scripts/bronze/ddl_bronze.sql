c3b80b1b68b3806f0f53bf178e5074d7bb207612
yessineallani <yessineallani4@gmail.com>
2025-11-24 09:14:20 +0100

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,3 +1,15 @@
+/*
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
+
+
 IF OBJECT_ID('bronze.DE_videos', 'U') IS NOT NULL
     DROP TABLE bronze.DE_videos;
 GO
